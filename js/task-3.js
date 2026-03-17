bdb853b4f72f804ac2e49ec13ba38b399f10e4e5
Vitalii_Zlotnyk <vitalikzlot12@gmail.com>
2026-03-17 16:50:13 +0100

    Complete 3-rd task

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+  const numContent = parseFloat(content);
+  const numPadding = parseFloat(padding);
+  const numBorder = parseFloat(border);
+
+  const totalWidth = numContent + numPadding * 2 + numBorder * 2;
+  return totalWidth;
+}
+
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
